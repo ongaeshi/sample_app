5f59886c096d9d7b360221e39b7d490bb5d9059d
ongaeshi <ongaeshi0621@gmail.com>
2014-05-17 18:28:55 +0900

    Add create to user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class UsersController < ApplicationController
 
   def show
@@ -6,5 +7,14 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存に成功
+    else
+      render 'new'
+    end
   end
 end
