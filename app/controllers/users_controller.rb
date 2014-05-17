fac1a8ae7d032c6872e043d939fb51f11a2b65d9
ongaeshi <ongaeshi0621@gmail.com>
2014-05-17 18:30:05 +0900

    Support Strong Parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       # 保存に成功
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
