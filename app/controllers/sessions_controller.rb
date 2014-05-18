f11af9cb2704e32a5b9ace47fcdecade899495ce
ongaeshi <ongaeshi0621@gmail.com>
2014-05-18 16:23:56 +0900

    Implment signing failed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      # Redirect to user#show
+    else
+      # Display a error message and re-render the signin form
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
