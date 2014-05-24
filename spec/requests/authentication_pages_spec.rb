bf51c0a9287f7fb99fccaa2ea2042f3c60c460d4
ongaeshi <ongaeshi0621@gmail.com>
2014-05-24 17:39:08 +0900

    Add users/ page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,6 +32,7 @@ describe "Authentication" do
       before { sign_in user }
 
       it { should have_title(user.name) }
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out'), href: signout_path }
@@ -68,6 +69,11 @@ describe "Authentication" do
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_title("Sign in") }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
 
         describe "submitting to the update action" do
