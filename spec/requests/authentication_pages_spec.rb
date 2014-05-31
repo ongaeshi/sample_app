98d1370ef55978cffdb6faea140c6cc57104971c
ongaeshi <ongaeshi0621@gmail.com>
2014-06-01 00:25:45 +0900

    9.6.8

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -79,13 +79,29 @@ describe "Authentication" do
 
       describe "when attempting to visit a protected page" do
         before do
-          sign_in user
           visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
         end
 
         describe "after signing in" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
